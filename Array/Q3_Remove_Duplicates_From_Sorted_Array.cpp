e900d7367f0eae508b24827803d2f4e83e210609
hashil7 <93893257+hashil7@users.noreply.github.com>
2024-07-15 06:39:21 +0530

    Create Q3_Remove_Duplicates_From_Sorted_Array.cpp

---
+++ Array/Q3_Remove_Duplicates_From_Sorted_Array.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int n = nums.size();
+        int left = 0, right = 1;
+        while (right < n) {
+            if (nums[left] == nums[right]) {
+                right++;
+
+            } else {
+                nums[++left] = nums[right++];
+            }
+        }
+        return left + 1;
+    }
+};
