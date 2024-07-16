58b019e3bab5aecce73a2952d72a63cb7be63025
hashil7 <93893257+hashil7@users.noreply.github.com>
2024-07-16 15:13:27 +0530

    Create Q7_Chocolate_Distribution.cpp

---
+++ Array/Day02/Q7_Chocolate_Distribution.cpp
@@ -0,0 +1,47 @@
+//LINK: https://www.geeksforgeeks.org/problems/chocolate-distribution-problem3825/1
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+    public:
+    long long findMinDiff(vector<long long> a, long long n, long long m){
+    sort(a.begin(),a.end());
+    int i=0,j=m-1;
+    long long  mini=LLONG_MAX;
+    while(j<n){
+        if(a[j]-a[i]<mini){
+            mini=a[j]-a[i];
+        }
+        j++;i++;
+    }
+    return mini;
+    
+    }   
+};
+
+//{ Driver Code Starts.
+int main() {
+	long long t;
+	cin>>t;
+	while(t--)
+	{
+		long long n;
+		cin>>n;
+		vector<long long> a;
+		long long x;
+		for(long long i=0;i<n;i++)
+		{
+			cin>>x;
+			a.push_back(x);
+		}
+		
+		long long m;
+		cin>>m;
+		Solution ob;
+		cout<<ob.findMinDiff(a,n,m)<<endl;
+	}
+	return 0;
+}
+// } Driver Code Ends
